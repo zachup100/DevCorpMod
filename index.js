649251d3d9d94f908984f2498da65f455f560c96
zachup100 <rwesthoff@ymail.com>
2018-09-29 00:12:47 -0400

    dwadawdwa

--- index.js
+++ index.js
@@ -22,6 +22,25 @@ var keywords = [
 
 rbx.login(username, password).then(function() {
   console.log("LoggedIn");
+
+  var onWallPost = rbx.onWallPost({group: groupId});
+  onWallPost.on("data", function(data) {
+	console.log("New wall post detected.")
+    var found = 0;
+    for (let phase of keywords) {
+      if (data.content.toLowerCase().search(phase) != -1) found+=5;
+    }
+    if (/\S+\.\S+/.exec(data.content)) {
+
+    }
+    if (found >= 5) {
+        console.log("Removing post with `" + data.content + "`` content by " + message.author.name + "!");
+      rbx.deleteWallPost({id:data.id, group:groupId}).catch(function(e) { });
+    }
+  });
+  onWallPost.on("close", function(e) { console.log("The event has disconnected!"); });
+  onWallPost.on("error", function(e) {});
+
   rbx.getWall({group: groupId}).then(function(data){
     var posts = data.posts
     for(var i = 0; i < data.posts.length; i++) {
@@ -41,23 +60,6 @@ rbx.login(username, password).then(function() {
     }
   }).catch(function(e) { console.log(e); });
 
-  var onWallPost = rbx.onWallPost({group: groupId});
-  onWallPost.on("data", function(data) {
-	console.log("New wall post detected.")
-    var found = 0;
-    for (let phase of keywords) {
-      if (data.content.toLowerCase().search(phase) != -1) found+=5;
-    }
-    if (/\S+\.\S+/.exec(data.content)) {
-
-    }
-    if (found >= 5) {
-        console.log("Removing post with `" + data.content + "`` content by " + message.author.name + "!");
-      rbx.deleteWallPost({id:data.id, group:groupId}).catch(function(e) { });
-    }
-  });
-  onWallPost.on("close", function(e) { console.log("The event has disconnected!"); });
-  onWallPost.on("error", function(e) {});
 }).catch(function(e) {
-console.log('Unexpected error:', e);
+  console.log('Unexpected error:', e);
 });
